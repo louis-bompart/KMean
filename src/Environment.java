b9aba41e69d0ebb76976b4108523192b508f9a40
Louis Bompart <louis.vincent.bompart@gmail.com>
2016-03-02 22:22:59 +0100

    Start development of the algorithm. (Startup by taking center randomly; Affecting all items in a category.)

--- src/Environment.java
+++ src/Environment.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 /**
  * Class where all the magic happens, the KMean algorithm is executed right here.
@@ -7,6 +10,9 @@ import java.util.ArrayList;
 
 public class Environment {
 
+    private final Distance distance = new EuclideanDistance();
+
+    private final int RANGE=100;
     /**
      * List of used categories for the run of K-means.
      */
@@ -26,4 +32,41 @@ public class Environment {
     }
 
     //TODO: Implements delegate methods of categories and dataSet as needed.
+
+    public void computeKMean() {
+        Random rnd = new Random();
+        Set<Item> centers = generateRandomCenter();
+    }
+
+    public Set<Item> generateRandomCenter() {
+        Random rnd = new Random();
+        Set<Item> tmp = new HashSet<>();
+        Set<Item> centers = new HashSet<>();
+        Random random = new Random();
+        for (Category category :
+                categories) {
+            Item item = dataSet.get(rnd.nextInt(dataSet.size()));
+            item.setCategory(category);
+            dataSet.remove(item);//Remove temporary the center of the dataSet to avoid having two centers for the same item.
+            tmp.add(item);
+            centers.add(item);
+        }
+        dataSet.addAll(tmp);
+        return centers;
+    }
+
+    public void affectAllItems(Set<Item> centers) {
+        for (Item item :
+                dataSet) {
+            Item nearest = null;
+            double min = Double.MAX_VALUE;
+            for (Item center :
+                    centers) {
+                double tmp = distance.computeDistance(center,item);
+                if(tmp<min) {
+                    item.setCategory(center.getCategory());
+                }
+            }
+        }
+    }
 }
